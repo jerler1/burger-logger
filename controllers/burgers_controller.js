68f4bd4fbefdeba97b9e6b1f29ff03b418d0f1c0
Bunks <busk_1@yahoo.com>
2021-01-26 21:04:58 -0500

    pulled the partials folder out of the layout folder

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -12,28 +12,62 @@ const burger = require("../models/burger");
 // =====================================================
 
 router.get("/", function (req, res) {
-  // Do something.
+  burger.all(function (allBurgers) {
+    const hbsObject = {
+      burgers: allBurgers,
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
 });
 
 // Post Routes
 // =====================================================
 
 router.post("/api/burgers/", function (req, res) {
-  // Do something.
+  burger.create(["name", "devoured"]),
+    [req.body.name, req.body.sleepy],
+    function (result) {
+      res.json({ id: result.insertId });
+    };
 });
 
 // Delete Routes
 // =====================================================
 
 router.delete("/api/burgers/:id", function (req, res) {
-  // Do something.
+  const condition = "id = " + req.params.id;
+
+  burger.delete(condition, function (result) {
+    if (result.affectedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
 });
 
 // Put Routes
 // =====================================================
 
-router.update("/api/burgers/:id", function (req, res) {
-  // Do something.
+router.put("/api/burgers/:id", function (req, res) {
+  const condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update(
+    {
+      devoured: req.body.devoured,
+    },
+    condition,
+    function (result) {
+      if (result.changedRows == 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    }
+  );
 });
 
 // Exporting the router object.
